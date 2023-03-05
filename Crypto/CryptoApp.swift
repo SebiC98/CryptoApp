d525fdcb360ef2d7525568bb8d293462057b5bb2
SebiC98 <sebastiancioata98@gmail.com>
2023-03-05 12:23:16 +0200

    first commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Sebastian Cioată on 05.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
